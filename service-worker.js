3bd291abcfaaba6efd5fd2a845d695cf0dbfa2e9
GitHub Action <action@github.com>
2026-06-01 10:05:59 +0000

    chore: auto-update manifest and service worker

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
    Cache-first strategy for full offline support
    ============================================ */
 
-const CACHE_NAME = 'neaea-vault-v23';
+const CACHE_NAME = 'neaea-vault-v24';
 
 // Core app shell
 const APP_SHELL = [
